58c641ce5e5383dbf1906615910faae3714e1959
Luis Quiroz <luisjqr_1990@hotmail.com>
2019-06-27 22:01:15 +0200

    Validaciones Login
    Validaciones Login

--- src/models/User.js
+++ src/models/User.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
-const bcrypt = require ('bcryptjs');
- 
 const {Schema} = mongoose;
+const bcrypt = require ('bcryptjs');
 
 const UserSchema = new Schema ({
 
     name: {type: String, required: true},
-    mail: { type: String, required: true},
-    pasword :{type: String, required: true},
+    email: { type: String, required: true},
+    password :{type: String, required: true},
     date: { type: Date, default: Date.now}
 
 });
@@ -24,4 +23,4 @@ UserSchema.methods.matchPassword = async function (password) {
 return await bcrypt.compare(password, this.password);
 };
 
-module.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model('User', UserSchema);
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-
+//Modelo
 const User = require('../models/User');
 
 router.get('/users/signin', (req, res) =>{
@@ -16,12 +16,8 @@ router.get('/users/signup', (req, res) =>{
 router.post('/users/signup', async (req, res) =>{
    const {name, email, password, confirm_password} = req.body;
    const errors =[];
-   //console.log(req.body)
-   if(name.length == 0){
+  if(name.length <= 0){
     errors.push({text: 'Please insert your name'});   
-   }
-   if(email.length == 0){
-       errors.push({text: 'Please insert your mail'});
    }
    if(password != confirm_password){
     errors.push({text: 'Password do not match'});   
@@ -29,23 +25,22 @@ router.post('/users/signup', async (req, res) =>{
    if (password.length <= 4){
        errors.push({text:'Password must be at least 4 characters'})
    }
-   if(errors.length >= 0){
-       res.render('users/signup', {errors, name, email, password, confirm_password});
+   if(errors.length > 0){
+       res.render('/users/signup', {errors, name, email, password, confirm_password});
    } else {
     const emailUser  =  await User.findOne({email: email});
-    if(emailUser){
-        req.flash('error_msg', 'The Email ir already in use');
-        res.redirect('users/signup');
-
-    }
-
+    if(emailUser) {
+        req.flash('error_msg', 'The Email is already in use.');
+        res.redirect('/users/signin');
+      } else {
+        
     const newUser = new User({name , email, password});
     newUser.password = await newUser.encryptPassword(password);
     await newUser.save();
     req.flash('success_msg','You are registered');
-    res.redirect('users/signin');
+    res.redirect('/users/signin');
    }
-
+   }
 });
 
 module.exports = router;
